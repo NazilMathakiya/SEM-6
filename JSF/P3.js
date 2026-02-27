8e763a6dcba7b14217e4d8bebeec0b523100c4bc
NazilMathakiya <mathakiyanazil03@gmail.com>
2026-02-27 10:16:07 +0530

    Update P3.js

--- JSF/P3.js
+++ JSF/P3.js
@@ -1,4 +1,4 @@
-// Write a Node.js program using moment to display the current date and time.
+13.3// Write a Node.js program using moment to display the current date and time.
 
 const moment = require("moment");
 
@@ -6,4 +6,5 @@ const now = moment();
 
 console.log("Current Date:", now.format("DD-MM-YYYY"));
 console.log("Current Time:", now.format("HH-mm-ss"));
+
 console.log("Full Date and Time:", now.format("DD-MM-YYYY HH-mm-ss"));
