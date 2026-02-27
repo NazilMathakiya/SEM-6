c0d0dc7d44f07764ffb485ed7a889be191009527
NazilMathakiya <mathakiyanazil03@gmail.com>
2026-02-27 10:14:16 +0530

    Add files via upload

---
+++ JSF/P1.js
@@ -0,0 +1,8 @@
+// Write a Node.js program using chalk to print colorful log messages
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('This is a blue message'));
+console.log(chalk.red('This is a red message'));
+console.log(chalk.green('This is a green message'));
+    
---
+++ JSF/P3.js
@@ -0,0 +1,9 @@
+// Write a Node.js program using moment to display the current date and time.
+
+const moment = require("moment");
+
+const now = moment();
+
+console.log("Current Date:", now.format("DD-MM-YYYY"));
+console.log("Current Time:", now.format("HH-mm-ss"));
+console.log("Full Date and Time:", now.format("DD-MM-YYYY HH-mm-ss"));
