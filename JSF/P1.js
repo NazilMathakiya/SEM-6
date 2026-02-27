49e23b4e65efa810d4a9aa1f64054ea59014d0c4
NazilMathakiya <mathakiyanazil03@gmail.com>
2026-02-27 10:14:46 +0530

    Re-add colorful log messages using chalk

--- JSF/P1.js
+++ JSF/P1.js
@@ -1,4 +1,4 @@
-// Write a Node.js program using chalk to print colorful log messages
+13.1// Write a Node.js program using chalk to print colorful log messages
 
 const chalk = require('chalk');
 
@@ -6,3 +6,4 @@ console.log(chalk.blue('This is a blue message'));
 console.log(chalk.red('This is a red message'));
 console.log(chalk.green('This is a green message'));
     
+
